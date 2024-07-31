ce3e4b694018cd88bc390ab157ceb29875116b30
Seohui Hyung <awe135@naver.com>
2024-08-01 08:49:15 +0900

    today

---
+++ src/day0731/D3_1209_Sum.java
@@ -0,0 +1,28 @@
+package day0731;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class D3_1209_Sum {
+	Scanner sc = new Scanner(System.in);
+	
+	int T = sc.nextInt();
+	
+	for(int test = 1; test <= T; test++) {
+		int[][] nemo = new int[10][10];
+		
+		// 배열에 숫자 입력
+		for(int r = 0; r < 10; r++) {
+			for(int c = 0; c < 10; c++) {
+				nemo[r][c] = sc.nextInt();
+			}
+		}
+		
+		List<Integer> arr = new ArrayList<>();
+		
+		
+		// 각 열의 합
+		
+	}
+}
