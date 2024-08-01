0c8b284608ea22e74fa16d5bd508d01bff599bb0
Seohui Hyung <awe135@naver.com>
2024-08-01 17:59:52 +0900

    august1

--- src/day0801/D3_1216_회문2.java
+++ src/day0801/D3_1216_회문2.java
@@ -10,58 +10,60 @@ public class D3_1216_회문2 {
 			int t  = sc.nextInt();
 			
 			// 100*100 배열 만들어서 입력받기
-			char[][]nemo1 = new char[100][100];
+			char[][]nemo = new char[100][100];
 			
 			for(int r = 0; r < 100; r++) {
 				String s = sc.next();
 				for(int c = 0; c < 100; c++) {
-					nemo1[r][c] = s.charAt(c);
+					nemo[r][c] = s.charAt(c);
 				}
 			}
 			
-			int max_len = 0;
+			int answer = 1;
 			
 			// 열에서 최대 회문 찾기
 			for(int r = 0; r < 100; r++) {
-				start : for(int start = 0; start < 100 - max_len; start++) {
-					int end = 99;
-					while(start + max_len <= end) {
-						boolean flag = checkPal(nemo[r], start, end);
-					}
-					for(int end = 99; end >= start; end--) {
-						if(nemo1[r][start] != nemo1[r][end]) {
+				int c = 0;
+				while(c <= 100-answer) {
+					for(int d = 100 - c; answer < d; d--) {
+						boolean palindrome = true;
+						for(int half = 0; half < d / 2; half++){
+							if(!(nemo[r][half+c] == (nemo[r][c+d-1-half]))) {
+							palindrome = false;
 							break;
-						} else {
-							int len = end - start+1;
-							if(len > max) {
-								max = len;
 							}
-							break;
+						}
+						if(palindrome) {
+							answer = Math.max(answer, d);
 						}
 					}
+					c++;
 				}
 			}
-			
+				
 			
 			// 행에서 최대 회문 찾기
 			for(int c = 0; c < 100; c++) {
-				for(int start = 0; start < 100; start++) {
-					for(int end = 99; end >= start; end--) {
-						if(nemo1[start][c] != nemo1[end][c]) {
+				int r = 0;
+				while(r <= 100-answer) {
+					for(int d = 100 - r; answer < d; d--) {
+						boolean palindrome = true;
+						for(int half = 0; half < d / 2; half++){
+							if(!(nemo[r+half][c] == (nemo[r+d-1-half][c]))) {
+							palindrome = false;
 							break;
-						} else {
-							int len = end - start+1;
-							if(len > max) {
-								max = len;
 							}
-							break;
+						}
+						if(palindrome) {
+							answer = Math.max(answer, d);
 						}
 					}
+					r++;
 				}
 			}
 			
 			
-			System.out.println("#" + t + " " + max);
+			System.out.println("#" + t + " " + answer);
 			
 			
 			
