d968bb5b4a2339b043feb95c4374f1b8956db4ec
JIEUN <awe135@naver.com>
2024-08-06 14:07:34 +0900

    academy:

--- src/d2/D2_1983_조교의성적매기기.java
+++ src/d2/D2_1983_조교의성적매기기.java
@@ -17,20 +17,43 @@ public class D2_1983_조교의성적매기기 {
 			int N = Integer.parseInt(line.nextToken());
 			int K = Integer.parseInt(line.nextToken());
 			
-			int[] score = new int[N];
+			double[] score = new double[N];
 			
 			for(int i = 0; i < N; i++) {
 				StringTokenizer line2 = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(line2.nextToken());
 				int b = Integer.parseInt(line2.nextToken());
 				int c = Integer.parseInt(line2.nextToken());
-				
-//				socre[i] = a*3.5 + b + c;
+
+				score[i] = (a * 0.35) + (b * 0.45) + (c * 0.2);
 			}
-			
-			
-			
-			System.out.println("#" + tc + " ");
+
+			//등수 구하기
+			int count = -1;
+
+			for(int i = 0; i < N; i++){
+				if(score[i] > score[K]){
+					count++;
+				}
+			}
+
+			double grade = Math.ceil(count/10);
+
+			String ans = switch (count) {
+                case 1 -> "A+";
+                case 2 -> "A0";
+                case 3 -> "A-";
+                case 4 -> "B+";
+                case 5 -> "B0";
+                case 6 -> "B-";
+                case 7 -> "C+";
+                case 8 -> "C0";
+                case 9 -> "C-";
+                default -> "D0";
+            };
+
+
+            System.out.println("#" + tc + " " + ans);
 		}
 		
 	}
