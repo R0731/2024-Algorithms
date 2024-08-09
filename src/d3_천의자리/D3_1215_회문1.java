2b6c1bce2e37616f6ea64b359e008377d50ea674
JIEUN <awe135@naver.com>
2024-08-09 17:45:10 +0900

    Academy: August 9

---
+++ src/d3_천의자리/D3_1215_회문1.java
@@ -0,0 +1,61 @@
+package d3_천의자리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class D3_1215_회문1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+
+        for(int tc = 1; tc <= 10; tc++){
+            int N = Integer.parseInt(br.readLine());
+
+            Character[][] nemo = new Character[8][8];
+
+            for(int r = 0; r < 8; r++){
+                String line2 = br.readLine();
+                char[] temp = line2.toCharArray();
+                for(int c = 0; c < 8; c++) {
+                    nemo[r][c] = temp[c];
+                }
+            }
+
+//            System.out.println(Arrays.deepToString(nemo));
+
+            int count = 0; //회문갯수를 세기 위한 변수
+
+            //가로 회문 찾기
+            for(int r = 0; r < 8; r++){
+                for(int c = 0; c <= 8 - N; c++) {
+                    boolean pel = true;
+                    for(int a = 0; a < N / 2; a++){
+                        if(nemo[r][c + a] != nemo[r][c + (N - 1 - a)]){
+                            pel = false;
+                            break;
+                        }
+                    }
+                    if(pel)
+                        count++;
+                }
+            }
+
+            //세로 회문 찾기
+            for(int c = 0; c < 8; c++){
+                for(int r = 0; r <= 8 - N; r++) {
+                    boolean pel = true;
+                    for(int a = 0; a < N / 2; a++){
+                        if(nemo[r + a][c] != nemo[r + (N - 1 - a)][c]){
+                            pel = false;
+                            break;
+                        }
+                    }
+                    if(pel)
+                        count++;
+                }
+            }
+
+            System.out.println("#" + tc + " " + count);
+        }
+    }
+}
---
+++ src/d3_천의자리/D3_1220_Magnetic.java
@@ -0,0 +1,51 @@
+package d3_천의자리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class D3_1220_Magnetic {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int tc = 1; tc <= 10; tc++){
+            int N = Integer.parseInt(br.readLine());
+
+            int[][] nemo = new int[N][N];
+
+            //네모 만들기
+            for(int r = 0; r < N; r++){
+                StringTokenizer line = new StringTokenizer(br.readLine());
+                for(int c = 0; c < N; c++){
+                    nemo[r][c] = Integer.parseInt(line.nextToken());
+                }
+            }
+
+            int deadlock = 0;
+
+            for(int c = 0; c < N; c++){
+                Stack<Integer> stack = new Stack<>();
+                for(int r = 0; r < N; r++){
+                    if(nemo[r][c] != 0) {
+                        stack.add(nemo[r][c]);
+                    }
+                }
+
+                while(stack.size() > 1){
+                    int a = stack.pop();
+                    int b = stack.peek();
+
+                    if(a == 2 && b == 1){
+                        deadlock++;
+                    }
+                }
+            }
+
+
+            System.out.println("#" + tc + " " + deadlock);
+        }
+    }
+}
