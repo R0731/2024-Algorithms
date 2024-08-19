9b005ea5af4659c309ba7d5478729b314bd4cf03
JIEUN <awe135@naver.com>
2024-08-19 17:56:43 +0900

    Academy: August 19

---
+++ src/d3_천의자리/D3_3260_두수의덧셈.java
@@ -0,0 +1,23 @@
+package d3_천의자리;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D3_3260_두수의덧셈 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int tc = 1; tc <= T; tc++){
+            BigInteger n1 = new BigInteger(sc.next());
+            BigInteger n2 = new BigInteger(sc.next());
+
+            BigInteger ans = n1.add(n2);
+
+            System.out.println("#" + tc + " " + ans);
+
+        }
+    }
+}
